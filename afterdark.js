e312610f8faab9cad28979ce3421406d6b9c587f
Wheel <36089272+TheWheelest@users.noreply.github.com>
2023-03-29 02:45:19 -0700

    Update afterdark.js

--- afterdark.js
+++ afterdark.js
@@ -1,17 +1,17 @@
 document.getElementById('inputform').onkeydown = function(e){
 if(e.keyCode == 13){
-    var userinput = inputform.value;
-    parseinput(userinput);
-    inputform.value="";
-    keysound2.currentTime = 0;
-    keysound2.play();
-    keysound2.volume = .25;
+		var userinput = inputform.value;
+		parseinput(userinput);
+		inputform.value="";
+		keysound2.currentTime = 0;
+		keysound2.play();
+		keysound2.volume = .25;
     }
-    else{
-    keysound.currentTime = 0;
-    keysound.volume = .25;
-    keysound.play();
-    }
+		else{
+		keysound.currentTime = 0;
+		keysound.volume = .25;
+		keysound.play();
+		}
     };
 function parseinput(){
     username.value="";
